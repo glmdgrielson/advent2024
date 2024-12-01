233b5205dc6e32c762ea50e5ef224fe0eb5405a7
glmdgrielson <zapperchamp1@gmail.com>
2024-12-01 11:26:18 -0500

    Add AdventError enum
    Off to a good start...

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+//! Helper systems for Advent of Code puzzles.
+
+use thiserror::Error;
+use std::io::Error as IOError;
+
+#[derive(Error, Debug)]
+pub enum AdventError {
+    #[error("Error reading file: {0}")]
+    File(#[from] IOError),
+}
