cf188e1a5ce8c2a3d2e3007e442cb4f5bb980a3c
glmdgrielson <zapperchamp1@gmail.com>
2024-12-03 13:01:02 -0500

    Solve part two of Day 03
    Well... that was a headache. Wish I had remembered regular expressions
    were a thing. (Can ya blame me for not thinking of a thing outside of
    the standard library?)

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -5,96 +5,119 @@
 //! with bad data and it's up to us to fix it.
 
 #![warn(clippy::all)]
-#![allow(dead_code)]
 
 use advent2024::AdventError;
+
 use std::fs::read_to_string;
+use std::sync::LazyLock;
+
+use regex::Regex;
+
+static INSTRUCTION_RE: LazyLock<Regex> = LazyLock::new(|| {
+    Regex::new(r"do\(\)|don't\(\)|mul\(\d+,\d+\)").expect("Invalid regex found")
+});
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-struct Multiply(u32, u32);
+/// The instructions the machine can perform.
+enum Instruction {
+    Multiply(u32, u32),
+    Enable,
+    Disable,
+}
 
-impl Multiply {
+impl Instruction {
     /// Perform the multiplication implied by this instruction.
-    fn compute(&self) -> u32 {
-        self.0 * self.1
+    fn compute(&self) -> Option<u32> {
+        match self {
+            Instruction::Multiply(one, two) => Some(one * two),
+            _ => None,
+        }
     }
 }
 
 /// Input consists of a chunk of memory.
 ///
-/// For right now, we only care about multiply instructions,
+/// The main focus of this puzzle is multiply instructions,
 /// which take the form of `mul(X,Y)` where X and Y are
 /// numbers to be multiplied together.
-fn parse_input(file: &str) -> Vec<Multiply> {
+///
+/// Important for part two are control flow instructions,
+/// which in this case are `do()` and `don't()`.
+///
+/// Syntax errors are to be expected, as this data is
+/// corrupted. As such we shouldn't fail when we see
+/// something we don't expect.
+fn parse_input(file: &str) -> Vec<Instruction> {
+    INSTRUCTION_RE
+        .find_iter(file)
+        .map(|m| m.as_str())
+        .map(|code| match code {
+            "do()" => Instruction::Enable,
+            "don't()" => Instruction::Disable,
+            mul => {
+                let Some(arg) = mul.strip_prefix("mul(") else {
+                    unreachable!("Invalid regext match {}", mul);
+                };
+                let Some((one, trail)) = arg.split_once(",") else {
+                    unreachable!("Invalid regex match {}", mul);
+                };
 
-    // Split the string by the instruction's prefix.
-    file.split("mul(").filter_map(|mem| {
-        let Some((one, trail)) = mem.split_once(",") else {
-            return None;
-        };
+                let Ok(one) = one.parse::<u32>() else {
+                    unreachable!("Invalid regex match {}", mul);
+                };
 
-        let Ok(one) = one.parse::<u32>() else {
-            return None;
-        };
+                let Some(two) = trail.strip_suffix(")") else {
+                    unreachable!("Invalid regex match {}", mul);
+                };
 
-        let Some((two, _)) = trail.split_once(")") else {
-            return None;
-        };
+                let Ok(two) = two.parse::<u32>() else {
+                    unreachable!("Invalid regex match {}", mul);
+                };
 
-        match two.parse::<u32>() {
-            Ok(two) => Some(Multiply(one, two)),
-            Err(_) => None,
-        }
-    }).collect()
-    // loop {
-    //     let Some(index) = memory.find("mul(") else {
-    //         // There are no more instructions in the string
-    //         // so we stop here.
-    //         break;
-    //     };
-    //
-    //     // Remove `mul(` prefix.
-    //     memory = &memory[index + 4..];
-    //
-    //     // Try to get the first number by itself.
-    //     let Some((one, trail)) = memory.split_once(",") else {
-    //         // The comma is missing, so
-    //         // there can't possibly be any
-    //         // more valid instructions.
-    //         break;
-    //     };
-    //     memory = trail;
-    //
-    //     // Check that we actually _have_ the first number.
-    //     let Ok(one) = one.parse::<u32>() else {
-    //         // Syntax error, oh well, let's move on.
-    //         continue;
-    //     };
-    //
-    //     let Some((two, trail)) = memory.split_once(")") else {
-    //         // We couldn't find the close paren
-    //         // so there can't be any more valid
-    //         // instructions. Break the loop.
-    //         break;
-    //     };
-    //     memory = trail;
-    //
-    //     let Ok(two) = two.parse::<u32>() else {
-    //         // Syntax error, oh well, try again.
-    //         continue;
-    //     };
-    //
-    //     muls.push(Multiply(one, two));
-    // }
+                Instruction::Multiply(one, two)
+            }
+        })
+        .collect()
 }
 
-fn part_one(data: &[Multiply]) -> u32 {
-    data.iter().map(|mul| mul.compute()).sum()
+/// Find the sum of the results of all of the multiply instructions.
+fn part_one(data: &[Instruction]) -> u32 {
+    data.iter().filter_map(|mul| mul.compute()).sum()
+}
+
+/// Find the sume of the enabled multiplications.
+///
+/// We can't just pay attention to the `mul` instructions,
+/// because we have to keep track of the state of the
+/// machine. `do()` and `don't()` respectively enable and
+/// disable the machine's ability to do multiplications.
+fn part_two(data: &[Instruction]) -> u32 {
+    let (_, out) = data
+        .iter()
+        .fold((true, 0), |(power, acc), &code| match code {
+            Instruction::Multiply(one, two) => match power {
+                true => (power, acc + one * two),
+                false => (power, acc),
+            },
+            Instruction::Enable => (true, acc),
+            Instruction::Disable => (false, acc),
+        });
+
+    out
 }
 
 fn main() -> Result<(), AdventError> {
     let file = read_to_string("src/input/day03.txt")?;
     let data = parse_input(&file);
+
+    println!(
+        "The sum of the multiply instructions is {}",
+        part_one(&data)
+    );
+    print!(
+        "The sum of the enabled multiply instructions is {}",
+        part_two(&data)
+    );
     Ok(())
 }
 
@@ -103,7 +126,7 @@ mod test {
     use super::*;
     use std::sync::LazyLock;
 
-    static INPUT: LazyLock<Vec<Multiply>> = LazyLock::new(|| {
+    static INPUT: LazyLock<Vec<Instruction>> = LazyLock::new(|| {
         let file = read_to_string("src/input/day03-test.txt").unwrap();
 
         parse_input(&file)
@@ -115,6 +138,22 @@ mod test {
 
         println!("{:?}", muls);
         assert_eq!(muls.len(), 4);
-        assert_eq!(muls[0], Multiply(2, 4));
+        assert_eq!(muls[0], Instruction::Multiply(2, 4));
+    }
+
+    #[test]
+    fn test_part_one() {
+        let muls = &*INPUT;
+
+        assert_eq!(part_one(&muls), 161);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let file = read_to_string("src/input/day03-test2.txt").unwrap();
+        let codes = parse_input(&file);
+
+        println!("{:?}", codes);
+        assert_eq!(part_two(&codes), 48);
     }
 }
