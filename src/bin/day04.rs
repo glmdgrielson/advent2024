908fcfc0e128bf192bab1e7fcaa9d2a2b534065b
glmdgrielson <zapperchamp1@gmail.com>
2024-12-04 11:14:35 -0500

    Solve part 2 of Day 04
    Alright, so the code works, except for my test case, which is
    _annoying_.

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -61,14 +61,17 @@ fn part_one(data: &Grid<Letter>) -> usize {
                     _ => None,
                 }
             });
-            let s_indices = a_indices.into_iter().filter_map(|(dir, idx)| {
-                let idx = find_neighbors(data, idx)[dir]?;
+            let s_indices = a_indices
+                .into_iter()
+                .filter_map(|(dir, idx)| {
+                    let idx = find_neighbors(data, idx)[dir]?;
 
-                match data.get(idx) {
-                    Some(Letter::S) => Some((dir, idx)),
-                    _ => None,
-                }
-            }).count();
+                    match data.get(idx) {
+                        Some(Letter::S) => Some((dir, idx)),
+                        _ => None,
+                    }
+                })
+                .count();
 
             s_indices
         })
@@ -96,11 +99,55 @@ fn find_neighbors<T>(grid: &Grid<T>, idx: GridIndex) -> Vec<Option<GridIndex>> {
         .collect()
 }
 
+/// Find the number of crosses in the grid.
+///
+/// A cross resembles the following:
+/// ```notrust
+/// M.S
+/// .A.
+/// M.S
+/// ```
+fn part_two(grid: &Grid<Letter>) -> usize {
+    grid.indices()
+        .filter(|&idx| grid[idx] == Letter::A)
+        .filter(|&idx| find_crosses(grid, idx))
+        .count()
+}
+
+/// Given an index, find the number of crosses it is a part of.
+fn find_crosses(grid: &Grid<Letter>, idx: GridIndex) -> bool {
+    // This is a pathological check. It _should_ never get 
+    // to this point, but just in case it does...
+    assert!(grid[idx] == Letter::A);
+
+    use Letter::*;
+
+    // Get the neighbors of this cell.
+    let neighbors = find_neighbors(grid, idx)
+        .iter()
+        .map(|idx| match idx {
+            Some(idx) => grid.get(*idx),
+            None => None,
+        })
+        .collect::<Vec<_>>();
+    // Get the lines of potential crosses.
+    let lines = [
+        [neighbors[0], neighbors[4]],
+        [neighbors[1], neighbors[5]],
+        [neighbors[2], neighbors[6]],
+        [neighbors[3], neighbors[7]],
+    ];
+
+    matches!(lines[1], [Some(M), Some(S)] | [Some(S), Some(M)])
+    && matches!(lines[3], [Some(M), Some(S)] | [Some(S), Some(M)])
+}
+
 fn main() -> Result<(), AdventError> {
     let file = read_to_string("src/input/day04.txt")?;
     let data = parse_input(&file)?;
 
     println!("The number of XMAS instances is {}", part_one(&data));
+    println!("The number of X-MAS instances is {}", part_two(&data));
     Ok(())
 }
 
@@ -130,5 +177,24 @@ mod test {
         let grid = &*INPUT;
 
         assert_eq!(part_one(&grid), 18);
+    }
+
+    #[test]
+    fn test_find_crosses() {
+        use Letter::*;
+
+        let cross = vec![X, M, X, M, A, S, X, S, X];
+        let grid = Grid::new(3, 3, cross);
+        let center = GridIndex::new(1, 1);
+        dbg!(grid[center]);
+
+        assert!(find_crosses(&grid, center));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let grid = &*INPUT;
+
+        assert_eq!(part_two(grid), 9);
     }
 }
