22a6abe21be382ededa6690759a11b35dae4e81d
glmdgrielson <zapperchamp1@gmail.com>
2024-12-05 10:09:11 -0500

    Document AdventError enum

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,9 @@ use std::io::Error as IOError;
 #[derive(Error, Debug)]
 pub enum AdventError {
     #[error("Error reading file: {0}")]
+    /// An error reading the file used as input.
     File(#[from] IOError),
+    /// An error in the format of the input file.
     #[error("Parsing error: {0}")]
     Parse(String),
 }
