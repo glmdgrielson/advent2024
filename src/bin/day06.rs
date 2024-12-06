17122f8883ec94a34a2e34b8258c7714dc1944f7
glmdgrielson <zapperchamp1@gmail.com>
2024-12-06 15:28:11 -0500

    Flesh out part 2 skeleton of Day 06
    ...I'm having that moment of "it works for others but not for me" and I
    hate it.

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -13,7 +13,7 @@ use advent2024::AdventError;
 
 use simple_grid::{Grid, GridIndex};
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 /// The direction a guard is facing.
 enum Direction {
     North,
@@ -22,7 +22,7 @@ enum Direction {
     West,
 }
 
-#[derive(Clone, PartialEq, Eq, Debug)]
+#[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
 /// A guard from the good old year of 1518.
 struct Guard {
     position: GridIndex,
@@ -145,14 +145,124 @@ fn part_one(guard: &Guard, grid: &Grid<bool>) -> usize {
 /// Find the number of points where a loop can
 /// be created in the guard's path.
 fn part_two(guard: &Guard, grid: &Grid<bool>) -> usize {
-    todo!()
+    let (taken, looped) = make_path(guard, grid);
+    assert!(!looped, "Invalid data");
+
+    taken
+        .iter()
+        .skip(1)
+        .filter(|&guard| {
+            let position = guard.position;
+            let start = match guard.direction {
+                Direction::North => grid.down_index(guard.position).unwrap(),
+                Direction::East => grid.right_index(guard.position).unwrap(),
+                Direction::South => grid.up_index(guard.position).unwrap(),
+                Direction::West => grid.right_index(guard.position).unwrap(),
+            };
+
+            let guard = Guard {
+                position: start,
+                direction: guard.direction,
+            };
+            let mut grid = grid.clone();
+            grid[position] = true;
+            make_path(&guard, &grid).1
+        })
+        .count()
+}
+
+fn make_path(guard: &Guard, grid: &Grid<bool>) -> (HashSet<Guard>, bool) {
+    let mut set = HashSet::new();
+    set.insert(*guard);
+
+    let mut guard = guard.clone();
+    loop {
+        match guard.direction {
+            Direction::North => match grid.up_cell(guard.position) {
+                Some(true) => {
+                    guard.direction = Direction::East;
+                    if !set.insert(guard) {
+                        return (set, true);
+                    }
+                }
+                Some(false) => {
+                    // This `.expect()` is safe since we _just_ proved it exists.
+                    let next = grid.up_index(guard.position).expect("Index should exist");
+                    guard.position = next;
+                    if !set.insert(guard) {
+                        return (set, true);
+                    }
+                }
+                None => break,
+            },
+            Direction::East => match grid.right_cell(guard.position) {
+                Some(true) => {
+                    guard.direction = Direction::South;
+                    if !set.insert(guard) {
+                        return (set, true);
+                    }
+                }
+                Some(false) => {
+                    let next = grid
+                        .right_index(guard.position)
+                        .expect("Index should exist");
+                    guard.position = next;
+                    if !set.insert(guard) {
+                        return (set, true);
+                    }
+                }
+                None => break,
+            },
+            Direction::South => match grid.down_cell(guard.position) {
+                Some(true) => {
+                    guard.direction = Direction::West;
+                    if !set.insert(guard) {
+                        return (set, true);
+                    }
+                }
+                Some(false) => {
+                    let next = grid.down_index(guard.position).expect("Index should exist");
+                    guard.position = next;
+                    if !set.insert(guard) {
+                        return (set, true);
+                    }
+                }
+                None => break,
+            },
+            Direction::West => match grid.left_cell(guard.position) {
+                Some(true) => {
+                    guard.direction = Direction::North;
+                    if !set.insert(guard) {
+                        return (set, true);
+                    }
+                }
+                Some(false) => {
+                    let next = grid.left_index(guard.position).expect("Index should exist");
+                    guard.position = next;
+                    if !set.insert(guard) {
+                        return (set, true);
+                    }
+                }
+                None => break,
+            },
+        }
+    }
+
+    (set, false)
 }
 
 fn main() -> Result<(), AdventError> {
     let file = read_to_string("src/input/day06.txt")?;
     let (guard, grid) = parse_input(&file)?;
 
-    println!("The guard passes through {} points", part_one(&guard, &grid));
+    println!(
+        "The guard passes through {} points",
+        part_one(&guard, &grid)
+    );
+    println!(
+        "{} possible ways to trap the guard",
+        part_two(&guard, &grid)
+    );
     Ok(())
 }
 
@@ -191,5 +301,36 @@ mod test {
         let (guard, grid) = &*INPUT;
 
         assert_eq!(part_two(guard, grid), 6);
+    }
+
+    #[cfg(test)]
+    fn add_block(grid: &Grid<bool>, point: GridIndex) -> Grid<bool> {
+        let mut grid = grid.clone();
+        grid[point] = true;
+        grid
+    }
+
+    #[test]
+    fn test_make_path() {
+        let (guard, grid) = &*INPUT;
+
+        assert!(
+            make_path(guard, &add_block(grid, (3, 6).into())).1,
+            "(3,6) does not loop"
+        );
+        assert!(
+            make_path(guard, &add_block(grid, (6, 7).into())).1,
+            "(6,7) does not loop"
+        );
+        assert!(
+            make_path(guard, &add_block(grid, (7, 7).into())).1,
+            "(7,7) does not loop"
+        );
+        assert!(
+            make_path(guard, &add_block(grid, (1, 8).into())).1,
+            "(1,8) does not loop"
+        );
+        assert!(make_path(guard, &add_block(grid, (3, 8).into())).1);
+        assert!(make_path(guard, &add_block(grid, (7, 9).into())).1);
     }
 }
