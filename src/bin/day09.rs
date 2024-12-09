e1d7ca0145852ee0d9a0710db01d6d92ce6451b3
glmdgrielson <zapperchamp1@gmail.com>
2024-12-09 16:14:45 -0500

    Revise part 1 solution of Day 09

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -10,7 +10,12 @@ use std::iter::repeat_n;
 use advent2024::AdventError;
 use itertools::Itertools;
 
-type Space = Option<usize>;
+// type Space = Option<usize>;
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+struct Space {
+    size: usize,
+    index: Option<usize>,
+}
 
 /// Input consists of a series of digits, representing
 /// the disk map of this bizarre computer setup.
@@ -26,15 +31,24 @@ fn parse_input(file: &str) -> Result<Vec<Space>, AdventError> {
             let one = one
                 .to_digit(10)
                 .ok_or_else(|| AdventError::Parse(format!("Invalid digit {}", one)))?;
-            let one = repeat_n(Some(idx), one as usize);
+            let one = one as usize;
 
             let two = two
                 .to_digit(10)
                 .ok_or_else(|| AdventError::Parse(format!("Invalid digit {}", two)))?;
-            let two = repeat_n(None, two as usize);
+            let two = two as usize;
 
             // Ok((0..one).map(|_| Some(idx)).chain((0..two).m))
-            Ok(one.chain(two))
+            Ok([
+                Space {
+                    size: one,
+                    index: Some(idx),
+                },
+                Space {
+                    size: two,
+                    index: None,
+                },
+            ])
         })
         .flatten_ok()
         .collect()
@@ -45,28 +59,44 @@ fn parse_input(file: &str) -> Result<Vec<Space>, AdventError> {
 /// First, defragment the computer by removing as much
 /// space as possible.
 fn part_one(data: &[Space]) -> usize {
-    let mut disk = data.to_vec();
-    let mut start = 0;
-    let mut end = data.len() - 1;
-
-    while start < end {
-        while disk[start].is_some() {
-            start += 1;
-        }
-        while disk[end].is_none() {
-            end -= 1;
-        }
-        if start < end {
-            disk.swap(start, end);
-        }
-        start += 1;
-        end -= 1;
-    }
+    let disk = defragment(data);
 
     disk.iter()
         .enumerate()
         .filter_map(|(idx, file)| file.map(|id| id * idx))
         .sum()
+}
+
+fn defragment(data: &[Space]) -> Vec<Option<usize>> {
+    let mut disk = data
+        .iter()
+        .flat_map(|file| repeat_n(file.index, file.size))
+        .collect::<Vec<_>>();
+    let mut start = 0;
+    let mut end = disk.len() - 1;
+
+    while start < end {
+        // Get the first free space.
+        while disk[start].is_some() {
+            start += 1;
+        }
+
+        // Get the last file
+        while disk[end].is_none() {
+            end -= 1;
+        }
+
+        // Check that we're not using space AFTER this file.
+        if start < end {
+            // Use the space.
+            disk.swap(start, end);
+        }
+
+        // Increment our counters.
+        start += 1;
+        end -= 1;
+    }
+    disk
 }
 
 fn main() -> Result<(), AdventError> {
@@ -91,12 +121,37 @@ mod test {
 
     #[test]
     fn test_parse_input() {
-        let data = &*INPUT;
+        let data = parse_input("12345").unwrap();
 
-        assert_eq!(data[0], Some(0));
-        assert_eq!(data[1], Some(0));
-        assert_eq!(data[2], None);
-        assert!(data[3].is_none());
+        assert_eq!(
+            data,
+            vec![
+                Space {
+                    size: 1,
+                    index: Some(0)
+                },
+                Space {
+                    size: 2,
+                    index: None
+                },
+                Space {
+                    size: 3,
+                    index: Some(1)
+                },
+                Space {
+                    size: 4,
+                    index: None
+                },
+                Space {
+                    size: 5,
+                    index: Some(2)
+                },
+                Space {
+                    size: 0,
+                    index: None
+                },
+            ]
+        );
     }
 
     #[test]
@@ -104,5 +159,21 @@ mod test {
         let data = &*INPUT;
 
         assert_eq!(part_one(&data), 1928);
+    }
+
+    #[test]
+    fn test_defragment() {
+        let data = &*INPUT;
+
+        let disk = defragment(&data)
+            .iter()
+            .filter_map(|file| *file)
+            .collect::<Vec<_>>();
+        assert_eq!(
+            disk,
+            vec![
+                0, 0, 9, 9, 8, 1, 1, 1, 8, 8, 8, 2, 7, 7, 7, 3, 3, 3, 6, 4, 4, 6, 5, 5, 5, 5, 6, 6
+            ]
+        )
     }
 }
